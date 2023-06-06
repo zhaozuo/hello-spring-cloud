73f3ad3c1204cc5e7f246226becac651d9bff0cc
Oxygen <1974077165@qq.com>
2023-06-06 22:31:57 +0800

    新建Zuul模块

---
+++ zuul/src/main/java/com/oxygen/zuul/ZuulApplication.java
@@ -0,0 +1,16 @@
+package com.oxygen.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+// 驱动Zuul代理启动
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+
+}
